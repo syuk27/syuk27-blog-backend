12af0559ea05be7f4911138d35f6b84cb9e7ad68
lilaye <lilaye@naver.com>
2025-03-10 22:30:53 +0900

    2025-03-10 jwt 인증 HttpOnly 쿠키 설정 추가

--- src/main/java/com/syuk27/blog/security/AuthenticationController.java
+++ src/main/java/com/syuk27/blog/security/AuthenticationController.java
@@ -3,24 +3,29 @@ package com.syuk27.blog.security;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
+@RequestMapping("/authenticate")
 public class AuthenticationController {
 
-	private final JwtTokenService tokenService;
+	private final JwtTokenService jwtTokenService;
 
 	private final AuthenticationManager authenticationManager;
 
-	public AuthenticationController(JwtTokenService tokenService, AuthenticationManager authenticationManager) {
-		this.tokenService = tokenService;
+	public AuthenticationController(JwtTokenService jwtTokenService, AuthenticationManager authenticationManager) {
+		this.jwtTokenService = jwtTokenService;
 		this.authenticationManager = authenticationManager;
 	}
 
-	@PostMapping("/authenticate")
-	public ResponseEntity<JwtTokenResponse> generateToken(@RequestBody JwtTokenRequest jwtTokenRequest) {
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody JwtTokenRequest jwtTokenRequest, HttpServletResponse response) {
 
 		var authenticationToken = new UsernamePasswordAuthenticationToken(
 				jwtTokenRequest.email(),
@@ -28,8 +33,16 @@ public class AuthenticationController {
 
 		var authentication = authenticationManager.authenticate(authenticationToken);
 
-		var token = tokenService.generateToken(authentication);
+		SecurityContextHolder.getContext().setAuthentication(authentication); // 인증 정보 저장
+		jwtTokenService.generateToken(response, authentication);
 
-		return ResponseEntity.ok(new JwtTokenResponse(token));
+		return ResponseEntity.ok().build();
+	}
+	
+	@PostMapping("/logout")
+	public ResponseEntity<?> logout(HttpServletResponse response) {
+		jwtTokenService.removeToken(response);
+		
+		return ResponseEntity.ok().build();
 	}
 }
